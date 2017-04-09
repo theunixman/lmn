5b4a66bdf43fd6eeedc950857cf4958e5e1ae4ad
Momo The Great <mo12g13@gmail.com>
2017-04-08 20:29:13 -0500

    Added some css to the login page and registration page to make it look a little better

--- LMNOPsite/lmn/views_notes.py
+++ LMNOPsite/lmn/views_notes.py
@@ -37,8 +37,13 @@ def new_note(request, show_pk):
 @login_required
 def edit_notes(request, pk):
     notes = get_object_or_404(Note, pk=pk)
-
-
+    form = NewNoteForm(request.POST or None, instance=notes)
+    if form.is_valid():
+        notes = form.save(commit=False)
+        notes.save()
+        return redirect('lmn:latest_notes')
+    else:
+        return render(request, r'lmn\notes\edit.html', {'form': form})
 
 def latest_notes(request):
     notes = Note.objects.all().order_by('posted_date').reverse()
