aff6e9977ebb2774d29d3f9393aa5eb475d78f5d
Brendon <brennon.mckeever@gmail.com>
2017-04-12 13:17:11 -0500

    try:

--- LMNOPproject/LMNOPsite/settings.py
+++ LMNOPproject/LMNOPsite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'LMNOPsite.lmn'
+    'LMNOPproject.lmn'
 ]
 
 MIDDLEWARE_CLASSES = [
