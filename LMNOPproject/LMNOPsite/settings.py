eee66963fff1f4fbd65ba6dcf0949c18a32f01c6
Brendon <brennon.mckeever@gmail.com>
2017-04-16 16:23:26 -0500

    trying to add new feature background_tasks to deployment

--- LMNOPproject/LMNOPsite/settings.py
+++ LMNOPproject/LMNOPsite/settings.py
@@ -41,8 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'background_task',
     'LMNOPproject.lmn',
-    'background_task'
 ]
 
 MIDDLEWARE_CLASSES = [
