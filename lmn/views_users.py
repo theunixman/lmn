2ce8b12fe853385fa95167801d7b46b3395d0740
Jack <jackxthexrabbit@gmail.com>
2018-04-08 15:41:20 -0500

    I added a link to edit user profile, but I keep getting a 'NoReverseMatch Error' I made it so if this works, it should display the registration form for now and then I'll add a form for editing profile.

--- lmn/views_users.py
+++ lmn/views_users.py
@@ -10,19 +10,27 @@ from django.contrib.auth import authenticate, login, logout
 from django.utils import timezone
 
 
-
 def user_profile(request, user_pk):
     user = User.objects.get(pk=user_pk)
     usernotes = Note.objects.filter(user=user.pk).order_by('posted_date').reverse()
     return render(request, 'lmn/users/user_profile.html', {'user' : user , 'notes' : usernotes })
 
 
-
 @login_required
 def my_user_profile(request):
-    # TODO - editable version for logged-in user to edit own profile
-    return redirect('lmn:user_profile', user_pk=request.user.pk)
 
+    if request.method == 'POST':
+
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user = authenticate(username=request.POST['username'], password=request.POST['password1'])
+            login(request, user)
+            return redirect('lmn:homepage')
+
+    else:
+        form = UserRegistrationForm()
+        return render(request, 'lmn/users/my_user_profile.html', {'form': form})
 
 
 def register(request):
@@ -36,11 +44,10 @@ def register(request):
             login(request, user)
             return redirect('lmn:homepage')
 
-        else :
+        else:
             message = 'Please check the data you entered'
-            return render(request, 'registration/register.html', { 'form' : form , 'message' : message } )
-
+            return render(request, 'registration/register.html', {'form': form, 'message': message})
 
     else:
         form = UserRegistrationForm()
-        return render(request, 'registration/register.html', { 'form' : form } )
+        return render(request, 'registration/register.html', {'form': form})
